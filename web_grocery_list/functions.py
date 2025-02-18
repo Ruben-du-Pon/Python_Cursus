8a42af0e784ae29f309e44e9e18c10aaff93261b
Ruben-du-Pon <rubenmdupon@gmail.com>
2025-02-18 15:39:31 +0100

    Added a default grocery list

--- web_grocery_list/functions.py
+++ web_grocery_list/functions.py
@@ -1,42 +1,44 @@
 
 FILEPATH = "list.txt"
+DEFAULT_GROCERIES = "default_groceries.txt"
 
 
-def get_todos(filepath: str = FILEPATH) -> list[str]:
-    """Read a text file and return each line as a list of to-do items.
+def get_list(filepath: str = FILEPATH) -> list[str]:
+    """Read a text file and return each line as a grocery list.
 
     Keyword Arguments:
         filepath -- The file to read (default: {"list.txt"})
 
     Returns:
-        A list with to-do items
+        A grocery list
     """
     with open(filepath, 'r') as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
+        list_local = file_local.readlines()
+    return list_local
 
 
-def write_todos(todo_list: list[str],
-                filepath: str = FILEPATH) -> None:
-    """Write the items in the todo_list to a file
+def write_list(grocery_list: list[str],
+               filepath: str = FILEPATH) -> None:
+    """Write the items in the grocery_list to a file
     Arguments:
-        todo_list -- A List of to-do items to write to a file
+        grocery_list -- A List of to-do items to write to a file
 
     Keyword Arguments:
         filepath -- The file to write the list to (default: {"list.txt"})
     """
-    todo_list = [item for item in todo_list]
     with open(filepath, 'w') as file_local:
-        file_local.writelines(todo_list)
+        file_local.writelines(grocery_list)
 
 
-def print_todos(todo_list: list[str]) -> None:
-    """"Prints out the current to-do list.
+def get_groceries(filepath: str = DEFAULT_GROCERIES) -> list[str]:
+    """Read a text file and return each line as a list of grocery items.
 
-    Arguments:
-        todo_list -- The to-do list to print.
+    Keyword Arguments:
+        filepath -- The file to read (default: {"default_groceries.txt"})
+
+    Returns:
+        A list with grocery items
     """
-    print("Current to-do list:")
-    for index, item in enumerate(todo_list):
-        row = f"{index +1}. {item.capitalize()}"
-        print(row.strip('\n'))
+    with open(filepath, 'r') as file_local:
+        groceries_local = file_local.readlines()
+    return groceries_local
--- web_grocery_list/web.py
+++ web_grocery_list/web.py
@@ -1,29 +1,41 @@
 import streamlit as st
-from functions import get_todos, write_todos
+from functions import get_list, write_list, get_groceries
 import os
 
+# Create the files if they don't exist
 if not os.path.exists("list.txt"):
     with open("list.txt", "w") as file:
         pass
+if not os.path.exists("default_groceries.txt"):
+    with open("default_groceries.txt", "w") as file:
+        pass
 
-todos = get_todos()
+grocery_list = get_list()
+groceries = get_groceries()
+
+with st.expander("Add grocery item"):
+    for grocery in groceries:
+        checkbox = st.checkbox(grocery, key=grocery)
+        if checkbox:
+            grocery_list.append(grocery)
+            write_list(grocery_list)
 
 
-def add_todo():
-    todo = st.session_state["new_todo"] + "\n"
-    todos.append(todo)
-    write_todos(todos)
+def add_groceries():
+    grocery = st.session_state["new_grocery"] + "\n"
+    grocery_list.append(grocery)
+    write_list(grocery_list)
 
 
 st.title("Groceries")
 
-for todo in todos:
-    checkbox = st.checkbox(todo, key=todo)
+for grocery in grocery_list:
+    checkbox = st.checkbox(grocery, key=grocery)
     if checkbox:
-        todos.remove(todo)
-        write_todos(todos)
-        del st.session_state[todo]
+        grocery_list.remove(grocery)
+        write_list(grocery_list)
+        del st.session_state[grocery]
         st.rerun()
 
 st.text_input(label="", placeholder="Add grocery item",
-              on_change=add_todo, key="new_todo")
+              on_change=add_groceries, key="new_grocery")
