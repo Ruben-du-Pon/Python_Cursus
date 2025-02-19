7d24f9601bfa2b0c139096ba95263e4946235415
Ruben-du-Pon <rubenmdupon@gmail.com>
2025-02-19 15:11:44 +0100

    Uploaded photo and andded a header

--- portfolio_app/main.py
+++ portfolio_app/main.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    st.image("images/photo.jpg")
 
 with col2:
     st.title("Ruben du Pon")
@@ -13,3 +13,9 @@ with col2:
     Hi, this is some text about me. I am a data scientist and I love to code.
     """
     st.info(content)
+
+content2 = """
+Below you can find some of the projects I have made.
+Feel free to contact me!
+"""
+st.write(content2)
