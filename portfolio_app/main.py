d9c4e3928d8027012bdeb7ef992370ce3fe64268
Ruben <rubenmdupon@gmail.com>
2025-02-21 15:57:02 +0100

    Adjusted size and centered image

--- portfolio_app/main.py
+++ portfolio_app/main.py
@@ -1,12 +1,35 @@
 import streamlit as st
 import pandas as pd
+from PIL import Image
 
 st.set_page_config(layout="wide")
 
-col1, empty_col, col2 = st.columns([1.5, 0.5, 1.5])
+col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.jpg")
+    image = Image.open("images/photo.jpg")
+    orig_width, orig_height = image.size
+    scale_factor = 0.25
+    new_width = int(orig_width * scale_factor)
+    new_height = int(orig_height * scale_factor)
+    resized_image = image.resize((new_width, new_height))
+    # Convert image to bytes
+    from io import BytesIO
+    import base64
+
+    buffered = BytesIO()
+    resized_image.save(buffered, format="PNG")
+    img_b64 = base64.b64encode(buffered.getvalue()).decode()
+
+    # Display centered image using HTML
+    st.markdown(
+        f"""
+        <div style="display: flex; justify-content: center;">
+            <img src="data:image/png;base64,{img_b64}" width="{new_width}" height="{new_height}">
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
 
 with col2:
     st.title("Ruben du Pon")
@@ -19,11 +42,12 @@ content2 = """
 Below you can find some of the projects I have made.
 Feel free to contact me!
 """
+st.write("")
 st.write(content2)
 
 df = pd.read_csv("data.csv", sep=";")
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, .25, 1.5])
 
 with col3:
     for index, row in df[0::2].iterrows():
