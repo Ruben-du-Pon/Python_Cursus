5b6044bad8de9ed2e59cfab6bf7b9aa08ea5d0b7
Ruben <rubenmdupon@gmail.com>
2025-02-21 15:18:54 +0100

    Added images

--- portfolio_app/main.py
+++ portfolio_app/main.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.set_page_config(layout="wide")
 
-col1, col2 = st.columns(2)
+col1, empty_col, col2 = st.columns([1.5, 0.5, 1.5])
 
 with col1:
     st.image("images/photo.jpg")
@@ -21,7 +21,7 @@ Feel free to contact me!
 """
 st.write(content2)
 
-df = pd.read_csv("data/data.csv", sep=";")
+df = pd.read_csv("data.csv", sep=";")
 
 col3, col4 = st.columns(2)
 
@@ -29,10 +29,12 @@ with col3:
     for index, row in df[0::2].iterrows():
         st.header(row["title"])
         st.write(row["description"])
-        st.image(row["image"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[1::2].iterrows():
         st.header(row["title"])
         st.write(row["description"])
-        st.image(row["image"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
