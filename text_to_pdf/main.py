e60edad53f1f5da4d33e6831494c413c90e96fa1
Ruben-du-Pon <rubenmdupon@gmail.com>
2025-02-27 12:19:48 +0100

    Added content

--- text_to_pdf/main.py
+++ text_to_pdf/main.py
@@ -1,4 +1,3 @@
-# import pandas as pd
 import glob
 from fpdf import FPDF
 from pathlib import Path
@@ -9,8 +8,16 @@ pdf = FPDF(orientation='P', unit='mm', format='A4')
 
 for filepath in filepaths:
     pdf.add_page()
+
     animal = Path(filepath).stem.title()
+
     pdf.set_font("Times", size=16, style='B')
     pdf.cell(w=50, h=8, txt=animal, ln=1)
+
+    with open(filepath, "r") as file:
+        content = file.read()
+
+    pdf.set_font("Times", size=12)
+    pdf.multi_cell(w=0, h=6, txt=content)
 
 pdf.output("pdf_files/animals.pdf")
