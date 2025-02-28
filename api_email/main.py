18f0ca5e6215fa70b9f9e96a7fce3465cc628f28
Ruben-du-Pon <rubenmdupon@gmail.com>
2025-02-28 12:22:13 +0100

    Created an app that sends an email with news items

---
+++ api_email/main.py
@@ -0,0 +1,34 @@
+import requests
+import os
+from dotenv import load_dotenv
+from send_email import send_email
+
+load_dotenv()
+
+topic = "tech"
+api_key = os.getenv("API_KEY")
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&" \
+      "sortBy=publishedAt&" \
+      f"apiKey={api_key}&" \
+      "language=en"
+
+request = requests.get(url)
+content = request.json()
+
+articles = content["articles"][:20]
+
+email_body = ""
+for article in articles:
+    email_body = email_body + article["title"] + "\n" + \
+        article["description"] + "\n" + article["url"] + 2*"\n"
+
+email_body = email_body.encode("utf-8")
+
+message = f"""\
+Subject: US Business News
+
+{email_body}
+"""
+
+send_email(message)
