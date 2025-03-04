77a2d15bfc800a9227805e1c07ab55730a16768d
Ruben-du-Pon <rubenmdupon@gmail.com>
2025-03-04 14:39:50 +0100

    Added data and temp calculation

--- weather_api/main.py
+++ weather_api/main.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import numpy as np
 from flask import Flask, render_template  # , request
 
 app = Flask(__name__)
@@ -10,7 +12,12 @@ def index():
 
 @app.route("/api/v1/<station>/<date>")
 def api(station, date):
-    return {"station": station, "date": date}
+    filename = "TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv("data/" + filename, skiprows=20, parse_dates=["    DATE"])
+    df['TG0'] = df['   TG'].mask(df[' Q_TG'] != 0, np.nan)
+    df['TG'] = df['TG0'] / 10
+    temperature = df.loc[df['    DATE'] == date]['TG'][0]
+    return {"station": station, "date": date, "temperature": temperature}
 
 
 if __name__ == "__main__":
