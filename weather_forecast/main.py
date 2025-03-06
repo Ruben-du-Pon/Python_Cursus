c38f63ce4fc2034d73b1016a2a3d4309fb432fbc
Ruben-du-Pon <rubenmdupon@gmail.com>
2025-03-06 13:33:29 +0100

    Added date and time to sky conditions

--- weather_forecast/main.py
+++ weather_forecast/main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import plotly.express as px
 from backend import get_data
+from datetime import datetime
 
 images = {"Clear": "images/clear.png",
           "Clouds": "images/cloud.png",
@@ -30,8 +31,33 @@ if place:
 
         if option == "Sky":
             sky_conditions = [d["weather"][0]["main"] for d in filtered_data]
-            st.image([images[condition] for condition in sky_conditions],
-                     width=115)
+            dates = [d["dt_txt"] for d in filtered_data]
+
+            # Process 8 items at a time (one day = 8 three-hour intervals)
+            for i in range(0, len(sky_conditions), 8):
+                # Get the next 8 items (or remaining items if less than 8)
+                day_conditions = sky_conditions[i:i + 8]
+                day_dates = dates[i:i + 8]
+
+                # Create 8 columns for this row
+                cols = st.columns(8)
+
+                # Fill the columns with available data
+                for col, condition, date in zip(cols,
+                                                day_conditions,
+                                                day_dates):
+                    with col:
+                        st.image(images[condition], width=115)
+
+                        time_string = datetime.strptime(
+                            date, "%Y-%m-%d %H:%M:%S").strftime("%d-%m %H:%M")
+                        st.markdown("<span style='font-size: 0.75em;"
+                                    "font-weight: bold;'>"
+                                    f"{time_string}</span>",
+                                    unsafe_allow_html=True)
+
+                # Add some spacing between rows
+                st.write("")
 
     except KeyError:
         st.info("That place is unknown.")
